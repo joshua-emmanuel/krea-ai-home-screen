4a452735f5116aea73ea665d44993beab7dc2571
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2025-09-19 16:05:17 +0100

    Add legal and pricing icon

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,13 @@ import Header from "@/components/header";
 import GenerateCardGrid from "@/components/generate-card-grid";
 import { ChevronDown } from "lucide-react";
 import ImageCarousel from "@/components/image-carousel";
+import { Button } from "@/components/ui/button";
+import LegalIcon from "@/components/icons/legal-icon";
+import PricingIcon from "@/components/icons/pricing-icon";
 
 export default function Home() {
   return (
-    <div className="xl:container px-6 md:px-8 mx-auto">
+    <div className="xl:container px-6 md:px-8 mx-auto relative">
       <Header />
 
       <section className="lg:mr-[-28] mt-20 mb-16">
@@ -23,7 +26,7 @@ export default function Home() {
         <GenerateCardGrid />
       </section>
 
-      <section>
+      <section className="mb-1">
         <div className="flex items-center justify-between">
           <h2 className="font-medium text-xl">Gallery</h2>
           <div className="flex gap-[0.1rem] items-center text-blue-600 text-xs font-medium">
@@ -32,6 +35,23 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      <div className="absolute bottom-2 right-0 flex gap-2">
+        <Button
+          size="sm"
+          className="flex items-center bg-muted text-neutral-700 dark:text-neutral-200 hover:bg-muted hover:text-neutral-700"
+        >
+          <LegalIcon />
+          <span className="hidden md:block">Legal</span>
+        </Button>
+        <Button
+          size="sm"
+          className="flex items-center bg-muted text-neutral-700 dark:text-neutral-200 hover:bg-muted hover:text-neutral-700"
+        >
+          <PricingIcon />
+          <span className="hidden md:block">Pricing</span>
+        </Button>
+      </div>
     </div>
   );
 }
---
+++ components/icons/legal-icon.tsx
@@ -0,0 +1,16 @@
+export default function LegalIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+      viewBox="0 0 16 16"
+      className="mb-0.5 inline-block h-4 w-4"
+    >
+      <path
+        fill="currentColor"
+        d="M16 1H4v2H0v9a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3zM4 12a1 1 0 1 1-2 0V5h2zm8-8v2H8V4zm0 3v2H8V7z"
+      ></path>
+    </svg>
+  );
+}
